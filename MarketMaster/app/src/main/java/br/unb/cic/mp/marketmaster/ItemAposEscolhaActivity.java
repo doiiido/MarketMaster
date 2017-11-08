ef7a22bef47f44424cf77739f05c1b311685256b
rodrigodmpa <rodrigo.demetrio.palma@gmail.com>
2017-11-08 16:46:23 -0200

    adicionado ultimas telas, parte 1 do projeto finalizada.

---
+++ MarketMaster/app/src/main/java/br/unb/cic/mp/marketmaster/ItemAposEscolhaActivity.java
@@ -0,0 +1,13 @@
+package br.unb.cic.mp.marketmaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemAposEscolhaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_apos_escolha);
+    }
+}
