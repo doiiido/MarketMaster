62a53935562161153370e57eef053968068bb780
Pedro Nogueira <140065032@aluno.unb.br>
2017-11-23 22:03:40 -0200

    continuando o caminho do aplicativo, com ruim na linha 19 da classe ListaActivity

--- MarketMaster/app/src/main/java/br/unb/cic/mp/marketmaster/GerenciarListasActivity.java
+++ MarketMaster/app/src/main/java/br/unb/cic/mp/marketmaster/GerenciarListasActivity.java
@@ -1,23 +1,26 @@
 package br.unb.cic.mp.marketmaster;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class GerenciarListasActivity extends AppCompatActivity {
 
-    private EditText mAddItem;
+    private FloatingActionButton mAddItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gerenciar_listas);
-        mAddItem = findViewById(R.id.floating_action_button);
+        mAddItem = findViewById(R.id.add_lista);
         mAddItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(GerenciarListasActivity.this, "Louco, mano.", Toast.LENGTH_SHORT);
                 Intent intent = new Intent(GerenciarListasActivity.this, AdicionarListaActivity.class);
                 startActivity(intent);
             }
--- MarketMaster/app/src/main/java/br/unb/cic/mp/marketmaster/ListaActivity.java
+++ MarketMaster/app/src/main/java/br/unb/cic/mp/marketmaster/ListaActivity.java
@@ -4,18 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class ListaActivity extends AppCompatActivity {
 
-    private EditText mAddItem;
+    private Button mAddItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
 
-        mAddItem = findViewById(R.id.add_item);
+        mAddItem = findViewById(R.id.add_item); // TODO deu ruim aqui... aparentemente mAddItem tá nulo e não consegue ser instanciado
         mAddItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
